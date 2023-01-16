74699551e96cbd2e2a644f02f7ed4e743e81c294
Dennis <spam@dennis-schmerbeck.de>
2023-01-16 19:34:18 +0100

    added logic for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,42 @@
+import { HttpService } from '@nestjs/axios/dist';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { map, Observable } from 'rxjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  api = `https://dummyjson.com`;
+
+  constructor(private readonly httpService: HttpService) {}
+
+  create(createUserDto: CreateUserDto): Observable<AxiosResponse> {
+    return this.httpService
+      .post(`${this.api}/users/add`, createUserDto)
+      .pipe(map((response) => response.data));
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Observable<AxiosResponse<User[]>> {
+    return this.httpService
+      .get(`${this.api}/users`)
+      .pipe(map((response) => response.data));
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): Observable<AxiosResponse<User>> {
+    return this.httpService
+      .get(`${this.api}/users/${id}`)
+      .pipe(map((response) => response.data));
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: number): Observable<AxiosResponse> {
+    return this.httpService
+      .delete(`${this.api}/users/${id}`)
+      .pipe(map((response) => response.data));
   }
 }
